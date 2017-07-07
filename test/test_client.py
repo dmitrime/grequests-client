1fce5f1553ba3c062fd4eee8c7820648a2e07393
Dmitri <dmitrim@leadiq.com>
2017-07-07 17:58:13 +0800

    use fixtures

--- test/test_client.py
+++ test/test_client.py
@@ -4,22 +4,28 @@ import sys
 sys.path.append('.')
 sys.path.append('..')
 
+from mock import patch
+from pytest import fixture
 from client import Client
 
-def test_url_sub_params():
+def test_url_sub_params(client):
     """ Params in the url are correctly substituted """
+    (c, _output) = client
+    assert c.urls[0] == _output[0]
+    assert c.data[0] == _output[1]
 
-    template = 'http://www.foo.com/{bar}/{baz}/bam'
-    params = {'bar': 'mybar', 'baz': 'ubaz', 'foo': 'onefoo'}
-    c = Client((template, params, id))
-    assert c.urls[0] == 'http://www.foo.com/mybar/ubaz/bam'
-    assert c.data[0] == {'foo': 'onefoo'}
 
-def test_url_no_sub():
-    """ Only get params are added (in sorted order) """
+@fixture
+def client(client_data):
+    return Client(client_data[0]), client_data[1]
 
-    template = 'http://www.foo.com'
-    params = {'foo': 'bar', 'x': 'y'}
-    c = Client((template, params, id))
-    assert c.urls[0] == 'http://www.foo.com'
-    assert c.data[0] == {'foo': 'bar', 'x': 'y'}
+@fixture(params = [
+        (('http://www.foo.com/{bar}/{baz}/bam', {'bar': 'mybar', 'baz': 'ubaz', 'foo': 'onefoo'}, id), 
+            ['http://www.foo.com/mybar/ubaz/bam', {'foo': 'onefoo'}]),
+
+        (('http://www.foo.com', {'foo': 'bar', 'x': 'y'}, id),
+            ['http://www.foo.com', {'foo': 'bar', 'x': 'y'}])
+    ]
+)
+def client_data(request):
+    return request.param
